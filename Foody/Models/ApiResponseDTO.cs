fd425903192c54ec98ce5a74416242e1cd82029a
Silvia.970 <silvia.sv.atanasova@gmail.com>
2025-08-14 20:49:16 +0300

    Add MyFoody file

---
+++ Foody/Models/ApiResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace Foody.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("foodid")]
+
+        public string? FoodId { get; set; }
+    }
+}
---
+++ Foody/Models/FoodDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace Foody.Models
+{
+    internal class IdeaDTO
+    {
+
+        [JsonPropertyName("name")]
+
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+
+        public string? Description { get; set; }
+
+        [JsonPropertyName("url")]
+
+        public string? Url { get; set; }
+    }
+
+}
